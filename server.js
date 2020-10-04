71c5fed9397da26f031cba208de20397316db354
Johnjohnn <conde2010@aol.com>
2020-10-04 16:38:06 -0400

    building the server

---
+++ server.js
@@ -0,0 +1,8 @@
+// first step WE NEED TO CREATE A SERVER 
+const http =  require('http')
+// server will need a function and 2 parameters request and response , the request parameter allows us to make request and the response allows us to 
+const server = http.createServer((request, response ) =>{
+
+
+
+})
