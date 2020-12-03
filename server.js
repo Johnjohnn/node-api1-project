14e1f0e488e3bc7a123d585d94661bf64442e3bf
johnjohnn <conde2010@aol.com>
2020-12-02 21:08:12 -0500

    creating database

--- server.js
+++ server.js
@@ -3,7 +3,11 @@ const express = require("express")
 const db = require("./database")
 
 
-const server =  express() 
+const server =  express()              // we are declaring express with this line 
+server.use(express.json())             // middleware that helps express parse incoming request bodies into json object 
+
+// start creating this endpoints after this line ======================
+
 
 
 
