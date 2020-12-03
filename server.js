c1d24c73639d5f8058ec50d5ac00361c10019e5a
johnjohnn <conde2010@aol.com>
2020-12-02 23:14:37 -0500

    finished

--- server.js
+++ server.js
@@ -38,20 +38,57 @@ server.get("/api/users:id", (req, res) => {
 
 //============================================================== POST API POINTS ======================================================================
 
-server.get("/api/users", (req, res) =>{
+server.post("/api/users", (req, res) =>{
     if(!req.body.name){
         return res.status(400).json({
             message:"Please provide name and bio for user"
         })
     }
+    const newUser =  db.createUser({
+        name: req.body.name,
+    })
+    res.json(newUser)
 })
 
 
+//============================================================== put API POINTS ======================================================================
+server.put("/api/users/:id", (req, res) => {
+    const user = db.getUserById(req.params.id)
 
+    if(user){
+        // if it exits then continue updating it 
+        const updateUser = db.updateUser( user.id, {
+            // have to be specific with values being updated,rather then just passing "req.body" directly
+            name:req.body.name || user.name
+        })
+        res.json(updateUser)
+    }else {
+        res.status(404).json({
+            message:"The user with the specific ID does not exist"
+        })
+    }
+})
 
+//============================================================== delete API POINTS ======================================================================
 
+server.delete("/api/users/:id", (req, res) =>{
+    const user = db.getUserById(req.params.id)
+    if (user) {
+		db.deleteUser(user.id)
 
+		// send back an "empty" successful response
+		res.status(204).end()
 
+		// or a success message
+		// res.json({
+		// 	message: "User deleted",
+		// })
+	} else {
+		res.status(404).json({
+			message: "User not found",
+		})
+	}
+})
 
 
 
