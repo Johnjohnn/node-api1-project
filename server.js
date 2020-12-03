abf4b66345c81df4130601ba6b78c81238aad0f6
johnjohnn <conde2010@aol.com>
2020-12-02 20:59:06 -0500

    adding packages

---
+++ server.js
@@ -0,0 +1,42 @@
+// import the modules 
+const express = require("express")
+const db = require("./database")
+
+
+const server =  express() 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// this is where the browser will listen to the API 
+server.listen(4000, () =>{
+    console.log("server started at http://localhost:4000")
+})
