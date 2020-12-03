b67702e0752fe7ad9f5648635c812e9868a9ed26
johnjohnn <conde2010@aol.com>
2020-12-02 22:05:18 -0500

    creating post request

--- database.js
+++ database.js
@@ -1,7 +1,7 @@
 let users = [
-	{ id: "1", name: "Jane Doe" },
-	{ id: "2", name: "John Doe" },
-	{ id: "3", name: "Jack Doe" },
+	{ id: "1", name: "Jonathan Munoz" },
+	{ id: "2", name: "Dohko de Libra " },
+	{ id: "3", name: "Asmita de Virgo" },
 ]
 
 function getUsers() {
--- server.js
+++ server.js
@@ -7,17 +7,44 @@ const server =  express()              // we are declaring express with this lin
 server.use(express.json())             // middleware that helps express parse incoming request bodies into json object 
 
 // start creating this endpoints after this line ======================
+//============================================================== GET API POINTS ======================================================================
+server.get("/api/users", (req, res) =>{
+    const users = db.getUsers()
+    res.json(users)
+    if (users){
+        res.json(users)
+    }else{
+        res.status(500).json({
+            errorMessage: "The users information could not be retrieved." 
+        })
+    }
+})
 
 
 
+server.get("/api/users:id", (req, res) => {
+  const id = req.params.id 
+  const user = db.getUserById(id)  
+
+  if(user){
+      res.json(user)
+  }else {
+      res.status(404).json({
+          message:"user with that ID is not found"
+      })
+  }
+})
 
 
+//============================================================== POST API POINTS ======================================================================
 
-
-
-
-
-
+server.get("/api/users", (req, res) =>{
+    if(!req.body.name){
+        return res.status(400).json({
+            message:"Please provide name and bio for user"
+        })
+    }
+})
 
 
 
